90d7d588692a442dd62b540e7682fba99a1265e6
Oskari Rauta <oskari.rauta@gmail.com>
2023-04-07 17:19:01 +0300

    add ubus_client.hpp
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

---
+++ include/ubus_client.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include "ubus.hpp"
+#include <json11.hpp>
+
+namespace ubus {
+
+	bool call(std::string path, std::string method, std::string msg, json11::Json& json);
+}
