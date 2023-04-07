7ec3eef17f8dbef8a423616aec064a50b0996283
Oskari Rauta <oskari.rauta@gmail.com>
2023-04-07 23:24:17 +0300

    add some more settings
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- include/settings.hpp
+++ include/settings.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace settings {
 
 	extern std::string wan_ifd;
+	extern bool auto_renew;
+	extern bool speed_testing;
+
+	extern std::vector<std::string> ping_hosts;
+
 }
--- src/settings.cpp
+++ src/settings.cpp
@@ -1,3 +1,7 @@
 #include "settings.hpp"
 
 std::string settings::wan_ifd = "wan";
+bool settings::auto_renew = true;
+bool settings::speed_testing = true;
+
+std::vector<std::string> settings::ping_hosts = { "8.8.8.8", "google.com" };
