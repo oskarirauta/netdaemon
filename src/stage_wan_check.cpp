056ba75e17f70630956f89b884a93c3345c24ac5
Oskari Rauta <oskari.rauta@gmail.com>
2023-04-08 23:33:04 +0300

    add some initial ubus calling
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- src/stage_wan_check.cpp
+++ src/stage_wan_check.cpp
@@ -1,4 +1,13 @@
+#include "json11.hpp"
+#include "settings.hpp"
+#include "ubus_client.hpp"
 #include "states.hpp"
 
 void states::wan_check(void) {
+
+	json11::Json js;
+	bool ret = ubus::call("network.interface." + settings::wan_ifd, "status", "", js);
+
+//	if ( !ret || json
+
 }
